96b35129d7e0a49ad2f470b1075f9cd9ac8edc43
Ching Chow <ching@getbento.com>
2019-02-19 10:10:05 -0500

    Automatically enter Python virtual env

--- profile_alias.sh
+++ profile_alias.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
 
+activate() {
+    if [ -d .venv ]
+    then
+        # shellcheck disable=SC1091
+        source .venv/bin/activate
+        echo "venv: $(command -v python)"
+    else
+        echo "venv not found"
+        return 1
+    fi
+}
+
+cd() {
+    builtin cd "$@" || return
+    if [ -d .venv ]
+    then
+        deactivate 2>/dev/null
+        activate
+    fi
+}
+
 is_macos() {
     if [ "$(uname -s)" == "Darwin" ]
     then
-        return 0  # this is an exit code; zero is success
+        return 0
     else
         return 1
     fi
+}
+
+mkvenv() {
+    python -m venv .venv
+    activate
 }
 
 reup() {
@@ -40,7 +66,3 @@ fi
 alias grep="grep -E --color"
 
 alias vi="vim"
-
-alias docker-dangles="docker images -f dangling=true -q"
-
-alias activate="source venv/bin/activate"
